e81c68126e6c394e0b2c4dc3f291cb89f41c43c1
daimaricel <daianamaricel@hotmail.com>
2023-05-15 00:15:22 -0300

    navbar con bootstrap

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -4,20 +4,33 @@ import "./NavBar.css"
 import logo from "../assets/Logo-dl.png"
 const NavBar = () => {
   return (
-    <header>
-       <h1><img src={logo} alt="" className="logoDl"/>
+      
+<nav className="navbar navbar-expand-lg">
+  <div className="container-fluid">
+   <h1><img src={logo} alt="" className="logoDl"/>
        alas Tennis Store</h1>
-        <nav>
-            <ul>
-                <li>Raquetas</li>
-                <li>Mochilas</li>
-                <li>Accesorios</li>
-                <li className="sale">SALE!</li>
-            </ul>
-        </nav>
-         <CartWidget/>
-
-    </header>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Raquetas</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Mochilas</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page"href="#">Accesorios</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active sale" aria-current="page" href="#">Sale!</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <CartWidget/>
+</nav>
   )
 }
 
