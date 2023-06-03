af3f69bc594864fc21c1c1076a6523a362b9032d
daimaricel <daianamaricel@hotmail.com>
2023-06-02 21:19:22 -0300

    firebase

---
+++ src/Services/config.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxczctRqRzd7NKmDQKjIE8XGWhaUl-IAk",
+  authDomain: "dalas-tennis-store.firebaseapp.com",
+  projectId: "dalas-tennis-store",
+  storageBucket: "dalas-tennis-store.appspot.com",
+  messagingSenderId: "38028859764",
+  appId: "1:38028859764:web:8135ace56b136488fd7421"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const bd = getFirestore(app);
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,22 +1,30 @@
 import "./ItemListContainer.css";
 import { useState, useEffect } from "react";
 import ItemList from "../ItemList/ItemList";
-import { getProductos, getProductosPorCategoria } from "../../asyncmock";
 import { useParams } from "react-router-dom";
+import {collection, getDocs, where, query} from "firebase/firestore";
+import { bd } from "../../Services/config";
+
 
 const ItemListContainer = ({ greeting }) => {
   const [productos, setProductos] = useState([]);
 
   const { idCat } = useParams();
 
-  useEffect(() => {
-    const funcionProductos = idCat ? getProductosPorCategoria : getProductos;
 
-    funcionProductos(idCat)
-      .then((res) => setProductos(res))
-      .catch((error) => console.log(error));
-  }, [idCat]);
-
+  useEffect( () =>{
+     const funcionProductos = idCat ? query (collection(bd, "productos" ), where ("idCategoria", "==", idCat)) : collection (bd, "productos"); 
+     
+     getDocs(funcionProductos)
+     .then (res =>{
+      const nuevosProductos = res.docs.map (doc => { 
+      const data =doc.data()
+      return{id: doc.id, ...data}
+     })
+      setProductos(nuevosProductos);
+    })
+    .catch(error =>console.log (error))
+    }, [idCat])
   return (
     <>
       <h2>{(greeting = "Bienvenidos a nuestra tienda virtual")}</h2>
